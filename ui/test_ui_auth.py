9316fc3a3ae1318de5688adb37fe7513d71621f7
amkatya <amkatya08@gmail.com>
2026-03-19 23:33:33 +0300

    style: fix code formatting, unused code removed

--- ui/test_ui_auth.py
+++ ui/test_ui_auth.py
@@ -2,12 +2,10 @@
 UI тесты аутентификации MiniBank
 Логин и логаут через пользовательский интерфейс
 """
-import time
 
 import pytest
 
 from config.settings import settings, UserRole
-from utils.api_client import MiniBankAPIClient
 from ui.pages.login_page import LoginPage
 from ui.pages.dashboard_page import DashboardPage
 
@@ -17,26 +15,22 @@ from ui.pages.dashboard_page import DashboardPage
 class TestUIAuthentication:
     """UI тесты аутентификации"""
 
-        
-
     def test_login_with_vip_user(self, driver):
         """Тест логина VIP пользователя"""
         login_page = LoginPage(driver)
         login_page.navigate_to()
         login_page.assert_page_loaded()
-        
+
         vip_user = settings.get_user(UserRole.VIP_USER)
         login_page.login(vip_user.email, vip_user.password)
-        
+
         dashboard_page = DashboardPage(driver)
         dashboard_page.assert_page_loaded()
-        
+
         current_url = driver.current_url
         assert "dashboard" in current_url.lower(), f"VIP user not on dashboard: {current_url}"
 
-
-
-    def test_basic_login(self, driver, api_client: MiniBankAPIClient):
+    def test_basic_login(self, driver):
         """Тест логина USER пользователя (по заданию 02)"""
         # Переходим на страницу логина
         login_page = LoginPage(driver)
@@ -53,9 +47,7 @@ class TestUIAuthentication:
         dashboard_page = DashboardPage(driver)
         dashboard_page.assert_page_loaded()
 
-
-
-    def test_login_logout_flow(self, driver, api_client: MiniBankAPIClient):
+    def test_login_logout_flow(self, driver):
         """Тест успешного логаута через UI (по заданию 02)"""
         # Переходим на страницу логина
         login_page = LoginPage(driver)
@@ -76,12 +68,9 @@ class TestUIAuthentication:
         dashboard_page.logout()
 
         # Проверяем что попали на страницу логина
-        login_page = LoginPage(driver)
         login_page.assert_page_loaded()
 
-
-
-    def test_login_with_wrong_password(self, driver, api_client: MiniBankAPIClient):
+    def test_login_with_wrong_password(self, driver):
         """Негативный тест входа с неправильным паролем (по заданию 02)"""
         # Переходим на страницу логина
         login_page = LoginPage(driver)
